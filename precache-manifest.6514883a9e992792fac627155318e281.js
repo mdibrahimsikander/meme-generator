a04d541f1503f0ba501f44cd5d88b2a092c273f7
Md Ibrahim Sikander <mdibrahimsikander@gmail.com>
2023-10-22 08:03:09 +0530

    Updates

---
+++ precache-manifest.6514883a9e992792fac627155318e281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdab6e6778a48d8c0fad40d77f9d435",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "14f3bb15b28fae384366",
+    "url": "/meme-generator/static/css/main.8dfeb7fc.chunk.css"
+  },
+  {
+    "revision": "6efa3a1d52800f90311d",
+    "url": "/meme-generator/static/js/2.d181343f.chunk.js"
+  },
+  {
+    "revision": "255f51076c8136fa5db6",
+    "url": "/meme-generator/static/js/3.27c15143.chunk.js"
+  },
+  {
+    "revision": "14f3bb15b28fae384366",
+    "url": "/meme-generator/static/js/main.0d40e110.chunk.js"
+  },
+  {
+    "revision": "794047dfa8ba9a3e1be9",
+    "url": "/meme-generator/static/js/runtime~main.087f7780.js"
+  }
+]);
